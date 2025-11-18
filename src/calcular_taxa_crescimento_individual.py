f9a8904babdadd34ee16df259c8cd052fc1f6653
Antonio <antoniojacintoa@gmail.com>
2025-11-18 16:59:25 -0300

    feat: criando funcao que calcula a taxa de crescimento individual

---
+++ src/calcular_taxa_crescimento_individual.py
@@ -0,0 +1,21 @@
+from src.obter_tendencias_emprego import obter_tendencias_emprego
+
+def calcular_taxa_crescimento_individual (url, headers, profissao):
+
+    antigas = obter_tendencias_emprego(url, headers, profissao, 10, "br", "month")   # Pega as vagas de emprego do último mês
+    recentes = obter_tendencias_emprego(url, headers, profissao, 10, "br", "all")  # Pega as vagas de emprego dos últimos tempos
+
+    quantidade_antigas = len(antigas)   # Pega quantas vagas tivemos há um mês
+    quantidade_recentes = len(recentes) # Pega quantas vagas tivemos há três dias
+
+    try:
+        # Calcula a taxa de crescimento
+        taxa = (quantidade_recentes - quantidade_antigas)/(quantidade_recentes + quantidade_antigas) * 100 
+
+        # Retorna a taxa calculada
+        return taxa
+    
+    except ZeroDivisionError:
+
+        # Retorna 0 se a divisão tiver 0 no denominador
+        return 0
